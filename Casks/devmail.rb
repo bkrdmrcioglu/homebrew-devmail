6ec26f59a23f20200dacb84cc4b6d76fa4851809
Bekir Demircioglu <bkrdmrcioglu@gmail.com>
2026-07-14 16:04:49 +0300

    Bump to 0.2.2

--- Casks/devmail.rb
+++ Casks/devmail.rb
@@ -1,8 +1,8 @@
 cask "devmail" do
-  version "0.2.1"
-  sha256 "8546410a6a9a76df3d7ed93c404786119dcea129c145255fdf0665b82d10f155"
+  version "0.2.2"
+  sha256 "a70a2e60406a8e91b9624f43e7cb56e954a2a188b09e7118242b273385d8d345"
 
-  url "https://github.com/bkrdmrcioglu/devmail-site/releases/download/v0.2.1/DevMail-0.2.1.zip"
+  url "https://github.com/bkrdmrcioglu/devmail-site/releases/download/v0.2.2/DevMail-0.2.2.zip"
   name "DevMail"
   desc "Part of DevSuites — see https://devsuites.dev/devmail/"
   homepage "https://devsuites.dev/devmail/"
