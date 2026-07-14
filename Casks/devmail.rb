045f587865106c9d8a7ceb50e07950b40172f7d6
Bekir Demircioglu <bkrdmrcioglu@gmail.com>
2026-07-14 11:05:49 +0300

    Set DevMail 0.1.0 notarized sha256.

--- Casks/devmail.rb
+++ Casks/devmail.rb
@@ -1,6 +1,6 @@
 cask "devmail" do
   version "0.1.0"
-  sha256 "REPLACE_AFTER_NOTARIZED_ZIP"
+  sha256 "f9ce6030d3e205e7ce937dcf2d3b5207de5d85eed13c92aa9f91d3a0e86d82dd"
 
   url "https://github.com/bkrdmrcioglu/devmail-site/releases/download/v#{version}/DevMail-#{version}.zip"
   name "DevMail"
